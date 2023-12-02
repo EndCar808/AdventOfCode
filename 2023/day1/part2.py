61c312e26097b7d0bd6a315e5731958d71dee22d
EndCar808 <enda.carroll@ucdconnect.ie>
2023-12-02 10:44:39 +0000

    Finished part 2

--- 2023/day1/part2.py
+++ 2023/day1/part2.py
@@ -1,9 +1,35 @@
-num_tot = 0
-# with open("input.txt") as infile:
-with open("test.txt") as infile:
-	## Read in lines from input file
-	for line in infile:
-		print(line.split())
+import re
+
+# word_digit_pairs = [
+#     ('zero', '0'),
+#     ('one', '1'),
+#     ('two', '2'),
+#     ('three', '3'),
+#     ('four', '4'),
+#     ('five', '5'),
+#     ('six', '6'),
+#     ('seven', '7'),
+#     ('eight', '8'),
+#     ('nine', '9')
+# ]
+
+# num_tot = 0
+
+# # with open("input.txt") as infile:
+# with open("test.txt") as infile:
+
+# 	## Read in lines from input file
+# 	for line in infile:
+# 		## Split line into text and numbers
+# 		ans = list(filter(None, re.split(r'(\d+)', line.split()[0])))
+
+# 		for i, elemnt in enumerate(ans):
+
+# 		print(ans)
+
+
+
+
 		# ## Loop through each character in the line
 		# num_tmp = []
 		# for c in line.strip():
@@ -19,4 +45,25 @@ with open("test.txt") as infile:
 		# print(num_tmp[0] + num_tmp[-1])
 		
 
-print(num_tot)
+# print(num_tot)
+# 
+#
+
+def compute_value(line):
+    first, last = None, None
+    for i, c in enumerate(line):
+        if c.isdigit():
+            first, last = first or int(c), int(c)
+        for d, w in enumerate(("one", "two", "three", "four", "five", "six", "seven", "eight", "nine"), 1):
+            if line[i: i + len(w)] == w:
+                first, last = first or d, d
+    return 10*first + last
+
+with open("input.txt") as infile:
+# with open("test.txt") as infile:
+	lines = []
+	for line in infile:
+		lines.append(line.strip())
+
+
+	print(sum(map(compute_value, lines)))
