85127ef61d23eceec17539930ed2e45204609ecd
EndCar808 <enda.carroll@ucdconnect.ie>
2023-12-06 12:02:15 +0000

    Attempt to day3

--- 2023/day3/part1.py
+++ 2023/day3/part1.py
@@ -1,6 +1,14 @@
-with open("input.txt") as infile:
+# with open("input.txt") as infile:
+with open("test.txt") as infile:
 	data = infile.read().splitlines()
 
 
 for line in data:
-	print(line.strip('.'))
+	# print(line)
+	# print(line.strip('.'))
+	# count=0
+
+	test = [(i, c) for i, c in enumerate(line) if c.isdigit()]
+	print(test)
+
+	# print(count)
